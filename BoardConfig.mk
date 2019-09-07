4d0f418024b8ef8157929ffa3ea98b8461928e59
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 11:13:09 -0700

    tornado: Build a disabled vbmeta image
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Change-Id: Ie8f1e0929b037b35b5e45c2e31be9253fa0bb097

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,6 +153,7 @@ BOOT_SECURITY_PATCH := 2026-02-01
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_ALGORITHM := SHA256_RSA2048
 
