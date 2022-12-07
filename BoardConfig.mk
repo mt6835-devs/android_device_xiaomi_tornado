5247e5411a23751e19004f03f356c8fed5fa1459
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-12-07 17:21:27 +0100

    tornado: Reboot to recovery if init fails
    Change-Id: I1d8f38620caa7b003911ff30f69ed7973c100e6d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 BOARD_KERNEL_CMDLINE += bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_DTB_OFFSET := 0x47c80000
