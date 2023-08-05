eebae0a18e27e364a0a98f5700aef1f8916bc170
Aayush Gupta <aayushgupta219@gmail.com>
2023-08-05 12:34:33 +0100

    tornado: Inherit sepolicy for mediatek devices
    There are lot of denials which are not device-specific. Henceforth,
    move such denials to a seperate sepolicy repository and inherit from it.
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I95c1f35006d49b797c617c5fea5ff27e5f98d45e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,6 +153,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 VENDOR_SECURITY_PATCH := 2026-02-01
 BOOT_SECURITY_PATCH := 2026-02-01
 
+# SELinux
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
