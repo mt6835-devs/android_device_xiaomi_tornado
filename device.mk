ba17ab8cab57cb9627fb85c03e8f1f000dfe5900
Lokesh Gidra <lokeshgidra@google.com>
2024-06-15 11:33:25 +0200

    tornado: Use 6GB Dalvik-heap Config
    Bug: 145823510
    Test: MPTS
    Change-Id: I99b04d088330ad465d2fc09986e975c262f2bdad

--- device.mk
+++ device.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
