3ae6495bc0bc7cb23f8e100181b55cc2245171e9
Sushrut1101 <guptasushrut@gmail.com>
2025-04-15 20:08:14 +0530

    tornado: Build common AIDL USB services

--- device.mk
+++ device.mk
@@ -90,5 +90,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
     hardware/mediatek
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/tornado/tornado-vendor.mk)
