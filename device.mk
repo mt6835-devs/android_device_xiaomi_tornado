b08bafe1fc9c8d1853b0b92a715cd85e4ea709ac
atharvnegi <atharvnegi2951@gmail.com>
2025-04-15 20:07:24 +0530

    tornado: Include hardware/mediatek in Soong Namespaces
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -87,7 +87,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
+    $(DEVICE_PATH) \
+    hardware/mediatek
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/tornado/tornado-vendor.mk)
