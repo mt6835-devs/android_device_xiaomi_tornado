bbc8f720dcaade46d18694e4681b6fc392ad748f
Alvyrion <alvyrion0@gmail.com>
2025-10-30 18:48:08 +0530

    tornado: Load `vendor_dlkm`, `vendor_ramdisk` kernel modules
    Signed-off-by: Alvyrion <alvyrion0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,12 @@ TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
 BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
 BOARD_MKBOOTIMG_ARGS += --dtb $(BOARD_PREBUILT_DTBIMAGE_DIR)/mt6835.dtb
 
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(KERNEL_PATH)/vendor_dlkm/*.ko)
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard $(KERNEL_PATH)/vendor_ramdisk/*.ko)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules.load.vendor_ramdisk))
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := tornado
 TARGET_NO_BOOTLOADER := true
